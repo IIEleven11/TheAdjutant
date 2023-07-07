3a77b88a7574a33fd54197490ac72ceb30af0319
IIEleven11 <jakemottola6@gmail.com>
2023-07-07 15:49:22 -0700

    Reworked DM, added bot to host. 95% functional

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from discord.ext import commands, tasks
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
 
+
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
 logging.getLogger("discord.http").setLevel(logging.INFO)
@@ -19,7 +20,7 @@ handler = logging.handlers.RotatingFileHandler(
     filename="discord.log",
     encoding="utf-8",
     maxBytes=32 * 1024 * 1024,
-    backupCount=5,  # Rotate through 5 files
+    backupCount=5,
 )
 dt_fmt = "%Y-%m-%d %H:%M:%S"
 formatter = logging.Formatter(
@@ -29,10 +30,7 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 
-# Discord bot token
-DISCORD_BOT_TOKEN = os.getenv("DICORD_BOT_TOKEN")
-
-# Google Calendar API credentials
+DISCORD_BOT_TOKEN = os.getenv("BOT_TOKEN_TESTER")
 SERVICE_ACCOUNT_CREDS = r"H:/PROGRAMS/metagpt/web/SERVICE_ACCOUNT_CREDENTIALS.json"
 GOOGLE_CALENDAR_ID = os.getenv("GOOGLE_CALENDAR_ID")
 
@@ -40,12 +38,16 @@ GOOGLE_CALENDAR_ID = os.getenv("GOOGLE_CALENDAR_ID")
 intents = discord.Intents.all()
 intents.message_content = True
 intents.members = True
-# Bot prefix
+
+
 bot = commands.Bot(command_prefix="!", intents=intents)
 events = []
 
+# set to keep track of responded message IDs(for sync command)
+responded_messages = set()
 
-# Manual command sync command, owner only, guild specific
+
+# Manual sync command, owner only, guild specific
 @bot.hybrid_command()
 async def sync(ctx):
     if str(ctx.author.id) == "498248765698867201":
@@ -56,23 +58,23 @@ async def sync(ctx):
         await ctx.send("Sorry, only the bot owner can execute this command.")
 
 
-# Load users' notification settings from JSON file
+# Load user from JSON file
 with open("userslist.json", "r") as file:
     users = json.load(file)
 
 
-# Helper function to save users' notification settings to JSON file
+#  save user notification to JSON file
 def save_users_list():
     with open("userslist.json", "w") as file:
         json.dump(users, file, indent=4)
 
 
-# Helper function to retrieve upcoming events from Google Calendar
+# retrieve upcoming events from Calendar
 def get_upcoming_events(max_results=10):
     with open("H:\PROGRAMS\metagpt\web\SERVICE_ACCOUNT_CREDENTIALS.json") as f:
         creds = Credentials.from_service_account_info(json.load(f))
     service = build("calendar", "v3", credentials=creds)
-    now = datetime.datetime.utcnow().isoformat() + "Z"  # 'Z' indicates UTC time
+    now = datetime.datetime.utcnow().isoformat() + "Z"
     events_result = (
         service.events()
         .list(
@@ -87,10 +89,7 @@ def get_upcoming_events(max_results=10):
     return events_result.get("items", [])
 
 
-# Helper function to send DM notification to users
-from html_parser import html_to_text
-
-
+# Sending of direct messages
 async def send_notification(user_id, event):
     print("send_notification function called")
     logger.info("send_notification function called")
@@ -111,16 +110,13 @@ async def send_notification(user_id, event):
             event_start_str = event_start.strftime(
                 "%B %d %Y at %I:%M %p"
             )  # Format the start time
-            event_details = event.get(
-                "description", ""
-            )  # Use an empty string if there are no details
+            event_details = event.get("description", "")
 
             # Convert HTML to markdown
             event_details = html_to_text(event_details)
 
             # Format the message
             message = f"Event Title: {event_title} at {event_start_str}\nEvent Details: {event_details}"
-
             await user.send(message)
             logger.info("Notification sent")
             print("Notification sent")
@@ -131,19 +127,18 @@ async def send_notification(user_id, event):
         logger.exception(f"An error occurred while sending notification: {str(e)}")
 
 
-# Discord bot event - Bot is ready
 @bot.event
 async def on_ready():
     print("Bot is ready")
     check_events.start()
 
 
-# Discord bot command - Opt In for notifications
+# Opt In
 @bot.hybrid_command()
 async def opt_in(ctx):
     user_id = ctx.author.id
 
-    # Check if user is already opted in
+    # Check if user is opted in
     if str(user_id) in users:
         await ctx.send("You are already opted in!")
     else:
@@ -152,7 +147,7 @@ async def opt_in(ctx):
         await ctx.send("You are now opted in for notifications!")
 
 
-# Discord bot command - Opt Out from notifications
+# Opt Out
 @bot.hybrid_command()
 async def opt_out(ctx):
     user_id = ctx.author.id
@@ -166,7 +161,9 @@ async def opt_out(ctx):
         await ctx.send("You are now opted out from notifications!")
 
 
-# Discord bot command - Set notification time
+from discord.ext import commands
+
+
 @bot.hybrid_command()
 async def set_notification(ctx, time_minutes: int):
     user_id = ctx.author.id
@@ -187,8 +184,46 @@ async def set_notification(ctx, time_minutes: int):
     save_users_list()
     await ctx.send(f"Notification time set to {time_minutes} minutes.")
 
+    # List of time zones
+    time_zones = [
+        "America/Los_Angeles",  # Pacific Time
+        "America/Denver",  # Mountain Time
+        "America/Chicago",  # Central Time
+        "America/New_York",  # Eastern Time
+        "Europe/London",  # Greenwich Mean Time
+        "Europe/Paris",  # Central European Time
+        "Asia/Dubai",  # United Arab Emirates Time
+        "Asia/Kolkata",  # Indian Standard Time
+        "Asia/Tokyo",  # Japan Standard Time
+        "Australia/Sydney",  # Australian Eastern Time
+    ]
 
-# Discord bot command - List upcoming events
+    # Ask for user's time zone
+    await ctx.send(
+        "Please reply with your time zone from the following options:\n"
+        + "\n".join(time_zones)
+    )
+
+    def check(m):
+        return (
+            m.author == ctx.author
+            and m.channel == ctx.channel
+            and m.content in time_zones
+        )
+
+    try:
+        msg = await bot.wait_for(
+            "message", check=check, timeout=60
+        )  # wait 60 seconds for user to reply
+    except asyncio.TimeoutError:
+        await ctx.followup.send("You didn't reply in time!")
+    else:
+        users[str(user_id)]["timezone"] = msg.content
+        save_users_list()
+        await ctx.followup.send(f"Time zone set to {msg.content}.")
+
+
+# List upcoming events
 @bot.hybrid_command()
 async def list_events(ctx):
     events = get_upcoming_events(max_results=7)
@@ -211,15 +246,17 @@ async def list_events(ctx):
     await ctx.send(output)
 
 
+# 1hr loop to fetch events
 @tasks.loop(minutes=60)
 async def fetch_events():
     global events
     events = get_upcoming_events(max_results=50)  # Increase max_results if needed
 
 
-@tasks.loop(minutes=5)
+# 3 minutes loop to send notifications
+@tasks.loop(minutes=3)
 async def check_notifications():
-    current_time = datetime.datetime.now(pytz.timezone("America/Los_Angeles"))
+    current_time = datetime.datetime.now(pytz.timezone("America/Indiana/Tell_City"))
 
     # Load users list
     with open("userslist.json") as f:
@@ -246,7 +283,7 @@ async def check_notifications():
 # Background task - Check and send notifications
 @tasks.loop(minutes=60)  # Adjust the interval according to your needs
 async def check_events():
-    current_time = datetime.datetime.now(pytz.timezone("America/Los_Angeles"))
+    current_time = datetime.datetime.now(pytz.timezone("America/Indiana/Tell_City"))
 
     # Iterate through users and their notification settings
     for user_id, settings in users.items():
@@ -267,6 +304,30 @@ async def check_events():
             event_start = datetime.datetime.fromisoformat(event["start"]["dateTime"])
             if event_start > current_time and event_start <= cutoff_time:
                 await send_notification(user_id, event)
+
+
+@bot.hybrid_command()
+async def embed(ctx):
+    embed = discord.Embed(
+        title="Usage",
+        description="**Step 1.** -__/Opt_in__\n"
+        "**Step 2.** - __/Set_Notification__\n"
+        "This is the amount of time prior to an event in which youd like to be notified. It must be within a range of 5 minutes to 5 days.*\n\n"
+        "**Other Commands**\n"
+        "- __/Opt_out__ -Remove yourself from the notification list\n\n"
+        "- __/List_Events__ - Lists the next 7 days worth of events.",
+        colour=0xD70909,
+        timestamp=datetime.datetime.now(),
+    )
+    embed.set_author(name="Adjutant", icon_url="https://imgur.com/15xTN3R.jpg")
+    embed.add_field(
+        name="Currently in beta",
+        value="*There will be some bugs to work out. Please let me know if you run into any issues.*",
+    )
+    embed.set_image(url="https://imgur.com/msNei0f.jpg")
+    embed.set_thumbnail(url="https://imgur.com/msNei0f.jpg")
+    embed.set_footer(text="The Adjutant/{}".format(ctx.author.display_name))
+    await ctx.send(embed=embed)
 
 
 async def main():
